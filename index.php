ce3d622cae94c7b5eff2fc8579b31d8b0fb49927
Ronal1509 <np03cs4s250031@heraldcollege.edu.np>
2026-05-13 10:01:34 +0545

    Add personalized AI food recommendations with fallback logic

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if (isLoggedIn() && hasRole('delivery_partner')) {
   <link
     href="https://fonts.googleapis.com/css2?family=Syne:wght@700;800&family=DM+Sans:wght@400;500;600;700&display=swap"
     rel="stylesheet" />
-  <link rel="stylesheet" href="assets/css/style.css?v=8" />
+  <link rel="stylesheet" href="assets/css/style.css?v=" />
 </head>
 
 <body>
